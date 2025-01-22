0e1542c3dea082156eef6d514f208de24a47a6bd
Pratham <prathu032@gmail.com>
2025-01-22 06:41:56 +0000

    updates

--- src/Basicnft.sol
+++ src/Basicnft.sol
@@ -1,4 +1,17 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Basicnft {}
+contract Basicnft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Doggie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {}
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
+}
